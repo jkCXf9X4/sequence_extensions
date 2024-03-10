7782c66334b4323b91a2e4bcc861a36af154a53a
Erik Rosenlund <erik.rosenlund.work@gmail.com>
2024-03-10 21:30:26 +0100

    Init

---
+++ extensions/__init__.py
@@ -0,0 +1,4 @@
+
+from extensions import ext_list
+
+__version__ = "0.1.0"
---
+++ extensions/__main__.py
@@ -0,0 +1,10 @@
+
+# __main__.py
+
+import extensions
+
+def main():
+    print("This package does not support to be run as a module")
+    
+if __name__ == "__main__":
+    main()
